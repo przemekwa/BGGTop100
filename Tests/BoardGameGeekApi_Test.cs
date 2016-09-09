5168b28527ac268d24c5ce7a92c5d2cf8f6ecf18
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-09-09 10:07:02 +0200

    Dodanienie projektu z testami i same testy

---
+++ Tests/BoardGameGeekApi_Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoardGameGeekApi;
+using Xunit;
+
+namespace Tests
+{
+    public class BoardGameGeekApi_Test
+    {
+        [Fact]
+        public void Get_Main_Page_Rank_Test()
+        {
+            var api = new BoardGameRank();
+
+            var result = api.GetRank(1, 100);
+
+            Assert.Equal(100, result.Count());
+        }
+
+        [Fact]
+        public void Get_Board_Game_Dto_Data_Test()
+        {
+            var api = new BoardGameRank();
+
+            var result = api.GetRank(1, 100).FirstOrDefault();
+
+            Assert.NotEmpty(result.Name);
+            Assert.Equal(1, result.Rank);
+        }
+
+        [Theory]
+        [InlineData(1,300)]
+        [InlineData(100,400)]
+        [InlineData(150,450)]
+        public void Get_Range_Rank_Test(object first, object last)
+        {
+            var api = new BoardGameRank();
+
+            var result = api.GetRank((short)first, (short)last);
+
+            Assert.Equal(100, result.Count());
+        }
+    }
+}
