6b06a6eca82e4426506891b26d1b235a1ee40f2d
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-09-09 14:49:26 +0200

    Dodanie projektu MVC do przegladania gier

---
+++ BoardGameRankExplorer/Controllers/ExploreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BoardGameGeekApi;
+using BoardGameRankExplorer.Models;
+
+namespace BoardGameRankExplorer.Controllers
+{
+    public class ExploreController : Controller
+    {
+        // GET: Explore
+        public ActionResult Index()
+        {
+            var api = new BoardGameRank();
+
+            var result =
+                api.GetRank(1, 200).Select(b => new BoardGameModel {Name = b.Name, Rank = b.Rank, Year = b.Year});
+            
+            return View(result);
+        }
+    }
+}
---
+++ BoardGameRankExplorer/Models/BoardGameModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoardGameRankExplorer.Models
+{
+    public class BoardGameModel
+    {
+        public string Name { get; set; }
+        public short Rank { get; set; }
+        public string Year { get; set; }
+    }
+}
